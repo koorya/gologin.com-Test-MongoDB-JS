ad0ac72b7c45cd6e2783fe9fc027cb2e426ba049
kirill <Puaktopblm@gmail.com>
2022-09-10 21:15:16 +0700

    Write result to third collection to the current database

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import { MongoClient } from "mongodb";
 import fs from "fs";
 import dJSON from "dirty-json";
 
-const dbName = process.env.DBNAME || "localhost";
+const dbHost = process.env.DBHOST || "localhost";
+
+const dbName = "db_name";
+const firstName = "first";
+const secondName = "second";
+const thirdName = "third";
 
 MongoClient.connect(
-  `mongodb://root:example@${dbName}:27017`,
+  `mongodb://root:example@${dbHost}:27017`,
   async (err, client) => {
     if (err) {
       console.log("Connection error: ", err);
@@ -14,18 +19,96 @@ MongoClient.connect(
 
     console.log("Connected");
 
-    await client.db("db_name").dropDatabase();
+    const db = await refillDB(client, dbName);
 
-    const db = client.db("db_name");
+    await createThirdCollection(db);
 
-    const companies = await db.createCollection("company");
-    const firstData = dJSON.parse(fs.readFileSync("data/first.json"));
-    await companies.insertMany(firstData);
+    const res = await db.collection(thirdName).find().toArray();
 
-    const queryParams = await db.createCollection("query_params");
-    const secondData = dJSON.parse(fs.readFileSync("data/second.json"));
-    await queryParams.insertMany(secondData);
+    console.log(res);
 
     client.close();
   }
 );
+
+const refillDB = async (client, dbName) => {
+  await client.db(dbName).dropDatabase();
+
+  const db = client.db(dbName);
+
+  const first = await db.createCollection(firstName);
+  const firstData = dJSON.parse(fs.readFileSync("data/first.json"));
+  await first.insertMany(firstData);
+
+  const second = await db.createCollection(secondName);
+  const secondData = dJSON.parse(fs.readFileSync("data/second.json"));
+  await second.insertMany(secondData);
+  return db;
+};
+
+const createThirdCollection = async (db) => {
+  const pipline = [
+    {
+      $addFields: {
+        longitude: {
+          $first: "$location.ll",
+        },
+        latitude: {
+          $last: "$location.ll",
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: secondName,
+        localField: "country",
+        foreignField: "country",
+        as: "output",
+      },
+    },
+    {
+      $addFields: {
+        diffs: {
+          $subtract: [
+            {
+              $sum: "$students.number",
+            },
+            {
+              $getField: {
+                field: "overallStudents",
+                input: {
+                  $first: "$output",
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$country",
+        count: {
+          $sum: 1,
+        },
+        longitude: {
+          $push: "$longitude",
+        },
+        latitude: {
+          $push: "$latitude",
+        },
+        allDiffs: {
+          $push: "$diffs",
+        },
+      },
+    },
+    {
+      $out: {
+        db: dbName,
+        coll: thirdName,
+      },
+    },
+  ];
+
+  await db.collection(firstName).aggregate(pipline).tryNext();
+};
